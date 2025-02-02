21fa403207b90bf85cc7ca58070ec9f78a655764
PVPGamingog <pvpgamingsubscribe@gmail.com>
2025-02-03 03:37:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".hover-image-wrapper").forEach(wrapper => {
+        let img = wrapper.querySelector("img");
+        let downloadBtn = document.createElement("a");
+        downloadBtn.href = img.src;
+        downloadBtn.download = img.src.split('/').pop();
+        downloadBtn.classList.add("download-btn");
+       
+        let icon = document.createElement("img");
+        icon.src = "download-icon.png"; 
+        icon.alt = "Download";
+        icon.classList.add("download-icon"); 
+
+        downloadBtn.appendChild(icon);
+        wrapper.appendChild(downloadBtn);
+    });
+});
